f72ba4c8966614b5afb2d591229e120db7a83995
David Garcia <David.GarOro@protonmail.com>
2023-07-21 19:00:05 -0600

    Add tension stuff

--- Catapult.gd
+++ Catapult.gd
@@ -16,13 +16,13 @@ func add_tension():
 	print("adding tension ", _tension)
 	_tension = min(_tension + 0.1,1.0)
 	arm.rotation_degrees.x = _tension*max_rot+(1-_tension)*min_rot
-	apply_force(transform*Vector3(0,-50,0), transform*Vector3(-0.5,0,-2))
+	apply_force(transform.basis*Vector3(0,-100,0), Vector3(0,0,-1.0))
 
 func release_tension():
 	print("releasing tension")
-	_tension = 0.0
 	arm.rotation_degrees.x = min_rot
-	apply_force(transform*Vector3(0,-40,300), transform*Vector3(0,2,5))
+	apply_force(transform.basis*_tension*Vector3(0,-400,600))
+	_tension = 0.0
 
 func _unhandled_input(event):
 	if event.is_action_pressed("ui_up"):
